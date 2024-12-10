f3afbc820911242b2c1c906f8a4964d9b0159ff0
Koalaila <antonio.jr.ramon@gmail.com>
2024-12-09 22:20:13 -0500

    Initial Commit

---
+++ PokePark2Tracker/main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PokePark2Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //TODO add main 
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# PokePark 2 Tracker
+## For Tracking Items, Friends, Areas, Areas Completed, and Story Progression
+
+**WIP will update as time comes**
